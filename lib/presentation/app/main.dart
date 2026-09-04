63bf203c745969d0e80d0f891a88690bf58901b2
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2026-09-04 21:03:04 +0330

    Refactor: Add main application entry point and initialize dependencies

--- lib/presentation/app/main.dart
+++ lib/presentation/app/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'data/remote/product_api_service.dart';
-import 'data/repositories/product_repository_impl.dart';
-import 'presentation/app/mini_shop_app.dart';
-import 'presentation/products/view_model/product_list_view_model.dart';
+import '../../data/remote/product_api_service.dart';
+import '../../data/repositories/product_repository_impl.dart';
+import 'mini_shop_app.dart';
+import '../products/view_model/product_list_view_model.dart';
 
 void main() {
   final httpClient = http.Client();
